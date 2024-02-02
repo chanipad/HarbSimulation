b8bda30bdd4deb5b40b074c5fcbfe27fdb251b7d
Abdulhadi <111729581+abdulhadi002@users.noreply.github.com>
2024-02-02 16:18:55 +0100

    shiptype

--- ClassLibrary.HarborFramwork/ShipInfo/ShipType.cs
+++ ClassLibrary.HarborFramwork/ShipInfo/ShipType.cs
@@ -13,5 +13,36 @@ namespace ClassLibrary.HarborFramwork.ShipInfo
         LEISURE_BOAT
     }
 
+    public class ShipSpeeds
+    {
+        public static int GetSpeed(ShipType shipType)
+        {
+            switch (shipType)
+            {
+                case ShipType.CARGO_SHIP:
+                    return "shipSpeed" = 20;
+                case ShipType.CRUISE_SHIP:
+                    return "shipSpeed" = 30;
+                case ShipType.LEISURE_BOAT:
+                    return "shipSpeed" = 50;
+                default:
+                    return null;
+            }
+        }
+    }
 
+    public static int travelTime(ShipType shipType, int distance)
+    {
+        int speed = ShipSpeeds.GetSpeed(shipType);
+
+        if (speed > 0)
+        {
+            int time = distance / speed;
+            return time;
+        }
+        else
+        {
+            return "Speed error!"
+        }
+    }
 }
