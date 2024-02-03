d5c132245e45f22f228f947c6b4dc9fa0e76330e
Abdulhadi <111729581+abdulhadi002@users.noreply.github.com>
2024-02-03 17:03:23 +0100

    shipcertifacte

--- ClassLibrary.HarborFramwork/ShipInfo/ShipCertificate.cs
+++ ClassLibrary.HarborFramwork/ShipInfo/ShipCertificate.cs
@@ -16,12 +16,27 @@
         public DateTime ExpirationDate { get; set; }
 
         /// <summary>
+        /// Gets or sets a flag indicating whether the ship has access control clearance.
+        /// </summary>
+        public bool HasAccessControlClearance { get; set; }
+
+        /// <summary>
+        /// Gets or sets a flag indicating whether the inspection of cargo and ship supplies has been performed.
+        /// </summary>
+        public bool IsInspectionPerformed { get; set; }
+
+        /// <summary>
         /// Validates the ship certificate.
         /// </summary>
         public static bool ValidateCertificate(ShipCertificate certificate)
         {
             // This check if the certificate is expired
-            return certificate.ExpirationDate > DateTime.Now;
+            if (certificate.ExpirationDate <= DateTime.Now)
+            {
+                return false;
+            }
+
+            return certificate.HasAccessControlClearance && certificate.IsInspectionPerformed;
         }
     }
 }
