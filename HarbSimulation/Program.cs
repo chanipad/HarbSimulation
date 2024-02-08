101d994fcfbf5d0063f969580c91c3fa8eb11081
Abdulhadi <111729581+abdulhadi002@users.noreply.github.com>
2024-02-08 14:36:00 +0100

    program.cs

--- HarbSimulation/Program.cs
+++ HarbSimulation/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace HarbSimulation
+﻿using System;
+using ClassLibrary.HarborFramework.ShipInfo;
+
+namespace HarbSimulation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Harbor Simulation!");
+
+            
+            Ship cargoShip = new Ship(1, ShipType.CARGO_SHIP);
+
+           
+            cargoShip.GetHistory().AddDocking(new Docking { DockSpace = "First place", Timestamp = DateTime.Now });
+            cargoShip.GetHistory().AddLoading(new Loading { Loading = "Cargo Area 1", Timestamp = DateTime.Now });
+
+            
+            ShipCertificate certificate = new ShipCertificate
+            {
+                CertificateID = 544,
+                ExpirationDate = DateTime.Now.AddDays(15),
+                HasAccessControlClearance = true,
+                IsInspectionPerformed = true
+            };
+
+            
+            bool isValid = ShipCertificate.ValidateCertificate(certificate);
+
+            
+            Console.WriteLine($"This Certificate is valid: {isValid}");
+
+      
+            cargoShip.GetHistory().DisplayHistory();
         }
     }
 }
