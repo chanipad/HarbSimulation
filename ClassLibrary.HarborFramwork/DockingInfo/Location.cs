8c2980632a00a02c455568d6da6d43dbbcff46e9
Chanipa <Chanipa@DESKTOP-NGEVS0V>
2024-02-12 17:28:27 +0100

    Dock Location with Timestamp to Program.cs

--- ClassLibrary.HarborFramwork/DockingInfo/Location.cs
+++ ClassLibrary.HarborFramwork/DockingInfo/Location.cs
@@ -4,26 +4,35 @@ namespace ClassLibrary.HarborFramework.DockingInfo
 {
     public class Location
     {
-        internal Location location;
+        public Location location;
+        private Location parentLocation;
+        internal object dockLocation;
+        internal DateTime timestamp;
+
+        public string DockLocation { get; private set; }
+        public DateTime Timestamp { get; private set; }
+        public DockSpace DockSpace { get; private set; }
+        public string NewLocation { get; private set; }
 
         //private TimeSlot Timestamp { get; set; }
-        private String DockLocation { get; set; }
+        /*private String DockLocation { get; set; }
         public Location NewLocation { get; set; }
         public DateTime Timestamp { get; set; }
 
-        /*public Location(int dockLocation, TimeSlot timeSlot)
+        public Location(int dockLocation, TimeSlot timeSlot)
         {
             DockLocation = dockLocation;
             TimeSlot = timeSlot;
         }
         */
-        public Location(string dockLocation, DateTime timeSlot)
+        public Location(string dockLocation, DateTime timeSlot, DockSpace dockSpace)
         {
             DockLocation = dockLocation;
             Timestamp = timeSlot;
+            DockSpace = dockSpace;
         }
 
-        public Location(string newLocation)
+        public Location(string newLocation, DateTime timestamp)
         {
             NewLocation = newLocation;
 
@@ -33,9 +42,27 @@ namespace ClassLibrary.HarborFramework.DockingInfo
         {
         }
 
-        public static implicit operator Location(string v)
+
+        public Location(string dockLocation, DateTime timestamp, Location parentLocation)
+        : this(dockLocation, timestamp)
+        {
+            this.parentLocation = parentLocation;
+        }
+
+        public Location(int v, DateTime now)
+        {
+        }
+
+        public static implicit operator Location(string newLocation)
         {
             throw new NotImplementedException();
         }
+
+
+        /*public static implicit operator Location(string v)
+        {
+            return new Location(v);
+        }*/
+
     }
 }
