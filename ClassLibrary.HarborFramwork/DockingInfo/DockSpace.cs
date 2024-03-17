8ab25562e6c68725fef02f1363cd30bbf325fe58
Granit <65214984+mentos1337@users.noreply.github.com>
2024-03-17 20:08:42 +0100

    small refactor

--- ClassLibrary.HarborFramwork/DockingInfo/DockSpace.cs
+++ ClassLibrary.HarborFramwork/DockingInfo/DockSpace.cs
@@ -95,7 +95,7 @@ namespace ClassLibrary.HarborFramework.DockingInfo
         /// <param name="vehicle">Transportmiddelet containeren skal lastes på.</param>
         /// <param name="scheduledTime">Tidspunktet for når lasteoperasjonen skal finne sted.</param>
         /// <returns>True hvis operasjonen var vellykket, ellers false.</returns>
-        public bool PerformLoadingOperation(Container container, VehicleType vehicle, DateTime scheduledTime)
+        public bool PerformLoadingOperation(Container container, Vehicle vehicle, DateTime scheduledTime)
         {
             var availableCrane = Cranes.FirstOrDefault(crane => crane.IsCraneAvailable(scheduledTime));
             if (availableCrane != null)
@@ -123,74 +123,5 @@ namespace ClassLibrary.HarborFramework.DockingInfo
         /// Tidspunktet for når skipet er planlagt for dokking.
         /// </summary>
         public DateTime DockingDateTime { get; set; }
-    }
-
-    /// <summary>
-    /// Representerer en kran som brukes til å laste containere på transportmidler.
-    /// </summary>
-    public class Crane
-    {
-        /// <summary>
-        /// Identifikatoren til kranen.
-        /// </summary>
-        public int CraneId { get; set; }
-
-        /// <summary>
-        /// Angir om kranen er tilgjengelig for operasjoner.
-        /// </summary>
-        public bool IsAvailable { get; set; } = true;
-
-        /// <summary>
-        /// En liste som holder styr på perioder hvor kranen er opptatt.
-        /// </summary>
-        private List<(DateTime Start, DateTime End)> BusySchedule { get; set; } = new List<(DateTime, DateTime)>();
-
-        public Crane(int craneId)
-        {
-            CraneId = craneId;
-        }
-
-        /// <summary>
-        /// Laster en container på et transportmiddel ved et planlagt tidspunkt hvis kranen er tilgjengelig.
-        /// </summary>
-        /// <param name="container">Containeren som skal lastes.</param>
-        /// <param name="vehicle">Transportmiddelet containeren skal lastes på.</param>
-        /// <param name="scheduledTime">Det planlagte tidspunktet for lasteoperasjonen.</param>
-        /// <returns>True hvis lasteoperasjonen ble utført, ellers false.</returns>
-        public bool LoadContainerOnTransport(Container container, ITransportVehicle vehicle, DateTime scheduledTime)
-        {
-            if (IsCraneAvailable(scheduledTime))
-            {
-                vehicle.LoadContainer(container);
-                Console.WriteLine($"Crane {CraneId} loaded container {container.ContainerId} onto vehicle.");
-                MarkCraneAsBusy(scheduledTime);
-                return true;
-            }
-            else
-            {
-                Console.WriteLine($"Crane {CraneId} is not available at the scheduled time.");
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// Sjekker om kranen er tilgjengelig på et gitt tidspunkt.
-        /// </summary>
-        /// <param name="time">Tidspunktet som skal sjekkes.</param>
-        /// <returns>True hvis kranen er tilgjengelig, ellers false.</returns>
-        public bool IsCraneAvailable(DateTime time)
-        {
-            return !BusySchedule.Any(interval => interval.Start <= time && interval.End >= time);
-        }
-
-        /// <summary>
-        /// Markerer kranen som opptatt for et gitt tidsintervall.
-        /// </summary>
-        /// <param name="time">Starttidspunktet for den opptatte perioden.</param>
-        public void MarkCraneAsBusy(DateTime time)
-        {
-            var busyPeriod = (Start: time, End: time.AddMinutes(30));
-            BusySchedule.Add(busyPeriod);
-        }
     }
 }
---
+++ ClassLibrary.HarborFramwork/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using ClassLibrary.HarborFramework.ContainerYardInfo;
+
+namespace ClassLibrary.HarborFramework
+{
+    public class Vehicle
+    {
+        public Enums.Vehicle Type { get; set; }
+
+        public Vehicle(Enums.Vehicle type)
+        {
+            Type = type;
+        }
+
+        public void LoadContainer(Container container)
+        {
+            Console.WriteLine($"Container {container.ContainerId} is loaded onto the {Type}.");
+        }
+
+        public void DeliverContainer(string destination)
+        {
+            Console.WriteLine($"{Type} is delivering the container to {destination}.");
+        }
+    }
+}
