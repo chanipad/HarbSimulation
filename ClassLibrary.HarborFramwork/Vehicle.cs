f046097558aafe2c4a58f4407b701394155091e2
Granit <65214984+mentos1337@users.noreply.github.com>
2024-03-20 13:50:37 +0100

    Update Vehicle.cs

--- ClassLibrary.HarborFramwork/Vehicle.cs
+++ ClassLibrary.HarborFramwork/Vehicle.cs
@@ -1,11 +1,13 @@
 ﻿using ClassLibrary.HarborFramework.ContainerYardInfo;
+using ClassLibrary.HarborFramwork.Exceptions;
+using System.Linq;
 
 namespace ClassLibrary.HarborFramework
 {
     public class Vehicle
     {
         public Enums.Vehicle Type { get; set; }
-
+        private List<Container> Containers { get; set; } = new List<Container>();
         public Vehicle(Enums.Vehicle type)
         {
             Type = type;
@@ -13,12 +15,21 @@ namespace ClassLibrary.HarborFramework
 
         public void LoadContainer(Container container)
         {
+            try
+            {
+                Containers.Add(container);
+            }
+            catch (Exception ex)
+            {
+                throw new ContainerYardCapacityExceededException($"Failed to load container on {Type}.", ex);
+            }
             Console.WriteLine($"Container {container.ContainerId} is loaded onto the {Type}.");
         }
 
-        public void DeliverContainer(string destination)
+        public Container DeliverContainer(string destination)
         {
             Console.WriteLine($"{Type} is delivering the container to {destination}.");
+            return Containers[0];
         }
     }
 }
